6086ee7a2fde7a8700df015960c959e71534a18b
Zioq <jr.roberthan@gmail.com>
2021-01-06 11:18:40 -0800

    update useremail and password validation and generate token

--- index.js
+++ index.js
@@ -1,43 +1,75 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const {User} = require("./models/User");
+const cookeParser = require("cookie-parser");
+const { User } = require("./models/User");
+
 
 const config = require("./config/key");
 
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
-
-mongoose.connect(config.mongoURI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true,
-    useFindAndModify:true
-}).then(()=> console.log(`MongoDB Connected`))
-  .catch(err=>console.log(err));
+//application/cookieParser
+app.use(cookeParser());
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => console.log(`MongoDB Connected`))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.post('/register',(req,res)=> {
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  const user = new User(req.body);
 
-    console.log(req.body);
-    const user = new User(req.body);
+  //Before save user data, encrypt password
 
-    //Before save user data, encrypt password
-    
-    user.save((err,userInfo)=> {
-        if(err) return res.json({success:false, err});
-        return res.status(200).json({success:true});
-    });
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
+app.post("/login", (req, res) => {
+  //Find email or username in DB
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "Failed to find user",
+      });
+    }
+    //If email is exists, check password too
+    user.comparePassword(req.body.password,(err, isMatch) => {
+        if (!isMatch) {
+          return res.json({ loginSuccess: false, message: "Wrong password" });
+        }
+
+        //If password also same, generate token
+        user.generateToken((err,user)=> {
+          if(err) return res.status(400).send(err);
+          // Save token into cookie
+            res.cookie("x_auth",user.token)
+            .status(200)
+            .json({loginSuccess:true,
+                   userId: user._id});
+        });
+      });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-
+const jwt = require("jsonwebtoken");
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -45,8 +45,31 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  } else {
+      next();
   }
 });
+
+//User password method
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword,this.password, function(err,isMatch) {
+        if(err) return cb(err);
+        cb(null,isMatch);
+    });
+};
+
+//User token method
+userSchema.methods.generateToken = function(cb) {
+
+    var user = this;
+    //Generate token by jsonwebtoken
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function(err,user) {
+        if(err) return cb(err);
+        cb(null, user);
+    });
+};
 
 const User = mongoose.model("User", userSchema);
 
